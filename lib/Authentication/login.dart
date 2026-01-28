d979eada9d399a7536388d175272a94ff674d08d
hoangtv04 <hoangtv004>
2026-01-28 18:12:42 +0700

    update SignUp done

--- lib/Authentication/login.dart
+++ lib/Authentication/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_expense/Authentication/signup.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -15,6 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   //1 bien de kiem tra hien thi va an mat khau
   bool isVisible = false;
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,83 +24,124 @@ class _LoginScreenState extends State<LoginScreen> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                //Email field
+            child: Form(
+              key: formkey,
+              child: Column(
+                children: [
+                  //Email field
 
-                //hien thi anh
-                Image.asset('lib/assets/login.png', width: 250),
-                const SizedBox(height: 15),
-                Container(
-                  margin: EdgeInsets.all(8),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 6,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.blueAccent.withOpacity(.2),
-                  ),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.email),
-                      border: InputBorder.none,
-                      hintText: "Email",
+                  //hien thi anh
+                  Image.asset('lib/assets/login.png', width: 250),
+                  const SizedBox(height: 15),
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 6,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.blueAccent.withOpacity(.2),
+                    ),
+                    child: TextFormField(
+                      controller: email,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter email';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.email),
+                        border: InputBorder.none,
+                        hintText: "Email",
+                      ),
                     ),
                   ),
-                ),
 
-                //Password field
-                Container(
-                  margin: EdgeInsets.all(8),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 6,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.blueAccent.withOpacity(.2),
-                  ),
-                  child: TextFormField(
-                    obscureText: !isVisible,
-                    decoration: InputDecoration(
-                      icon: Icon(Icons.lock),
-                      border: InputBorder.none,
-                      hintText: "Password",
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          //In here we will add functionality to show/hide password
-                          setState(() {
-                            // nut bam
-                            isVisible = !isVisible;
-                          });
-                        },
-                        icon: Icon(
-                          isVisible ? Icons.visibility : Icons.visibility_off,
+                  //Password field
+                  Container(
+                    margin: EdgeInsets.all(8),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 6,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.blueAccent.withOpacity(.2),
+                    ),
+                    child: TextFormField(
+                      controller: password,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'password is required';
+                        }
+                        return null;
+                      },
+                      obscureText: !isVisible,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.lock),
+                        border: InputBorder.none,
+                        hintText: "Password",
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            //In here we will add functionality to show/hide password
+                            setState(() {
+                              // nut bam
+                              isVisible = !isVisible;
+                            });
+                          },
+                          icon: Icon(
+                            isVisible ? Icons.visibility : Icons.visibility_off,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
 
-                const SizedBox(height: 10),
-                // nut login
-                Container(
-                  height: 55,
-                  width: MediaQuery.of(context).size.width * .9,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.blueAccent,
-                  ),
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      "LOGIN",
-                      style: TextStyle(color: Colors.white),
+                  const SizedBox(height: 10),
+                  // nut login
+                  Container(
+                    height: 55,
+                    width: MediaQuery.of(context).size.width * .9,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.blueAccent,
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        if (formkey.currentState!.validate()) {
+                          //nếu đúng thì làm gì đó
+                        }
+                      },
+                      child: const Text(
+                        "LOGIN",
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
-                ),
-              ],
+
+                  // nut dang ky
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don't have an account?"),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Signup(),
+                            ),
+                          );
+                        },
+                        // thanh dang ky
+                        child: const Text("Sign Up"),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
